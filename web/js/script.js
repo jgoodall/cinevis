18f92aa495d5e5ae959865b3e06c49e5766c12ec
John Goodall <jgoodall@ornl.gov>
2012-01-24 14:54:26 -0500

    configurable color scales for categorical and numeric data

--- web/js/script.js
+++ web/js/script.js
@@ -24,20 +24,43 @@
 */
 
 
-// by default, x = budget, y = gross
+// axes: by default, x = budget, y = gross
 var xField = 'Budget',
     yField = 'Worldwide Gross';
 var xScale, yScale,
     xAxis, yAxis,
     xAxisPanel, yAxisPanel,
     xLegend, yLegend;
-var colorField = 'Story';
-var colorScale = d3.scale.category20c();
 
+// color: scales for numeric fields
+var redBlue = ["rgb(103, 0, 31)", "rgb( 178, 24, 43)", "rgb( 214, 96, 77)", "rgb( 244, 165, 130)", "rgb( 253, 219, 199)", "rgb( 209, 229, 240)", "rgb( 146, 197, 222)", "rgb( 67, 147, 195)", "rgb( 33, 102, 172)", "rgb( 5, 48, 97)"];
+var brownBlueGreen = ["rgb(84, 48, 5)", "rgb( 140, 81, 10)", "rgb( 191, 129, 45)", "rgb( 223, 129, 125)", "rgb( 246, 232, 195)", "rgb( 199, 234, 229)", "rgb( 128, 205, 193)", "rgb( 53, 151, 143)", "rgb( 1, 102, 94)", "rgb( 0, 60, 48)"];
+var purpleGreen = ["rgb(64, 0, 75)", "rgb( 118, 42, 131)", "rgb( 153, 112, 171)", "rgb( 194, 165, 207)", "rgb( 231, 212, 232)", "rgb( 217, 240, 211)", "rgb( 166, 219, 160)", "rgb( 90, 174, 97)", "rgb( 27, 120, 55)", "rgb( 0, 68, 27)"];
+var redYellowBlue = ["rgb(165, 0, 38)", "rgb( 215, 48, 39)", "rgb( 244, 109, 67)", "rgb( 253, 174, 97)", "rgb( 254, 224, 144)", "rgb( 224, 243, 248)", "rgb( 171, 217, 233)", "rgb( 116, 173, 209)", "rgb( 69, 117, 180)", "rgb( 49, 54, 149)"];
+var purpleOrange = ["rgb(127, 59, 8)", "rgb( 179, 88, 6)", "rgb( 224, 130, 20)", "rgb( 253, 184, 99)", "rgb( 254, 224, 182)", "rgb( 216, 218, 235)", "rgb( 178, 171, 210)", "rgb( 128, 115, 172)", "rgb( 84, 39, 136)", "rgb( 45, 0, 75)"];
+var palettes = [redBlue, brownBlueGreen, purpleGreen, redYellowBlue, purpleOrange],
+    currentPaletteIndex = 0,
+    currentPalette = palettes[currentPaletteIndex];
+
+var colorField = 'Story';
+var categoricalFields = ['Story', 'Genre', 'Lead Studio'];
+var categoricalColorScale = d3.scale.category20c();
+// need to set domain of numeric scale: domain([minVal, mean, maxVal])
+var numericColorScale = d3.scale.quantile().range(currentPalette);
+var colorScale = $.inArray(colorField, categoricalFields) >= 0 ? categoricalColorScale : numericColorScale;
+
+
+// TODO - these probably shouldnt be globals
 var data;
 var svg;
 
+// utility functions
 var numberFormat = d3.format(',.0f');
+var isNumber = function (n) {
+  return !isNaN(parseFloat(n)) && isFinite(n);
+};
+
+// getter/setters
 
 var domain = function(axis, field) {
   if ( arguments.length > 1 ) {
@@ -63,7 +86,7 @@ var locate = function(d, axis) {
   return $.isNumeric(datum) ? scale(datum) : domain(axis)[0];
 }
 
-
+// load the data asynchronously
 d3.json('/data/moviedata.json', function(json) {
   data = json;
 
@@ -183,9 +206,12 @@ function mouseout(d, i) {
 
 // execute when dom is ready
 $( function() {
+  // set controls to be defaults
   $('#xaxis').val(xField);
   $('#yaxis').val(yField);
+  $('#color').val(colorField);
 
+  // listen for changes to axis and color controls
   $('#xaxis').change(function() {
     xField = $('#xaxis').val();
     domain('x', xField);
@@ -194,7 +220,6 @@ $( function() {
     svg.select('#xTicks').call(xAxis);
     svg.select('#xLabel').text(xField);
     redraw();
-
   });
   $('#yaxis').change(function() {
     yField = $('#yaxis').val();
@@ -204,7 +229,12 @@ $( function() {
     svg.select('#yTicks').call(yAxis);
     svg.select('#yLabel').text(yField);
     redraw();
-
+  });
+  $('#color').change(function() {
+    colorField = $('#color').val();
+    colorScale = $.inArray(colorField, categoricalFields) >= 0 ? categoricalColorScale : numericColorScale.domain([d3.min(data, function(d) {return d[colorField];}), d3.sum(data, function(d) {return d[colorField];}) / data.length, d3.max(data, function(d) {return d[colorField];})]);
+    // TODO - update color legend
+    redraw();
   });
 
 });
