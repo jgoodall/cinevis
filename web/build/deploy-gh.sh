4dd3cfc93f3df89554d9bf4bbb2184f99798d780
John Goodall <jgoodall@gmail.com>
2012-01-27 13:15:53 -0500

    script to copy web site up to github

---
+++ web/build/deploy-gh.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+# To be run from build dir
+# This will deploy the minified site to github page
+
+ant minify
+cp -rp ../publish/ ../../../gh-pages/
+cd ../../../gh-pages/
+git commit -a -m 'rebuild'
+git push github-http gh-pages
