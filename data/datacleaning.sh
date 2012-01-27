539479f63d65f83c9ced2f2afd272e736b6cc593
John Goodall <jgoodall@gmail.com>
2012-01-27 11:14:52 -0500

    still working on cleaning data - cvskit may be the problem

--- data/datacleaning.sh
+++ data/datacleaning.sh
@@ -3,13 +3,13 @@
 # Preprocessing in raw data before running this script
 #  * removed extra rows at the end of each sheet
 #  * added 'average' row to 2011 sheet
-#  * made profitability formula and format consistent throughout sheets
+#  * made profitability formula and format consistent throughout sheets - format needs to remove the comma separator (and made to 4 decimal places)
 
 
 # Clean the csv files
 unset years
 unset files
-for i in `ls *csv` ; do yr=`echo $i | cut -d'.' -f1` ; years="${yr} ${years}" ; file=$yr-clean.csv; files="${file} ${files}" ; cat $i | sed 's/,Film ,/,Film,/' | sed 's/,Audience  score %,/,Audience Rating,/' | sed 's/,Rotten Tomatoes %,/,Critic Rating,/' | sed 's/,Number of Theatres in US Opening Weekend,/,Opening Weekend Theaters,/' | sed 's/,Opening Weekend,/,Opening Weekend Revenue,/' | sed 's/,Box Office Average per US Cinema (Opening Weekend),/,Opening Weekend per Cinema,/' | sed '/^y/d' | sed '/^,,,,,/d' | sed '/% of budget recovered/d' | csvcut -c 2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17 > $file ; done
+for i in `ls *csv` ; do yr=`echo $i | cut -d'.' -f1` ; years="${yr} ${years}" ; file=$yr-clean.csv; files="${file} ${files}" ; cat $i | sed 's/,Film ,/,Film,/' | sed 's/,Audience  score %,/,Audience Rating,/' | sed 's/,Rotten Tomatoes %,/,Critic Rating,/' | sed 's/,Number of Theatres in US Opening Weekend,/,Opening Weekend Theaters,/' | sed 's/,Opening Weekend,/,Opening Weekend Revenue,/' | sed 's/,Box Office Average per US Cinema (Opening Weekend),/,Opening Weekend per Cinema,/' | sed '/^y/d' | sed '/^,,,,,/d' | sed '/% of budget recovered/d' | sed 's/ ,/,/' | csvcut -c 2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17 > $file ; done
 years=`echo $years | tr ' ' ','`
 
 # Concatenate all files into a single CSV
